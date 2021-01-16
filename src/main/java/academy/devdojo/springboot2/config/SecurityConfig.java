bc27fd6487629f4a310d2c0c06aa7ca3839cd55a
Pablo Satler <pablosatler@gmail.com>
2021-01-16 12:29:43 -0300

    feat: install spring web security
    - also add some additional web security configuration such as basic authentication and creation of users in memory

---
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package academy.devdojo.springboot2.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.log4j.Log4j2;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .httpBasic();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    PasswordEncoder passwordEncoder= PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    log.info("Password encoded {}", passwordEncoder.encode("test"));
+
+    auth.inMemoryAuthentication()
+            .withUser("AdminUser")
+            .password(passwordEncoder.encode("admin-pass"))
+            .roles("USER", "ADMIN")
+            .and()
+            .withUser("devdojo")
+            .password(passwordEncoder.encode("devdojo-pass"))
+            .roles("USER");
+  }
+}
